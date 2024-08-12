1b520807834faf5b4461fee3d32f8198307b7b32
Featherbutt <iamjohndoe220@gmail.com>
2024-08-12 14:49:03 -0700

    Update default value of `max_organ_level` to 5, which matches the base game.

--- LiesOfPRandomizer/Skills/SkillConfig.cs
+++ LiesOfPRandomizer/Skills/SkillConfig.cs
@@ -11,7 +11,7 @@ public class SkillConfig : Module.Config
     public Config.ShuffleMode skill_slots { get; set; } = Config.ShuffleMode.DONT_RANDOMIZE;
     public  Config.ShuffleMode skill_nodes { get; set; } = Config.ShuffleMode.DONT_RANDOMIZE;
     public bool include_amulets {  get; set; } = false;
-    public uint max_organ_level {  get; set; } = 7;
+    public uint max_organ_level {  get; set; } = 5;
 
     public bool remove_chapter_1_level_cap { get; set; } = false;
     public bool max_slots_per_node { get; set; } = false;
