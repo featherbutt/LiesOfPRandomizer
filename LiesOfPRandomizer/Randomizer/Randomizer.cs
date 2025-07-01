15874a0139953253312827fcce256bf0a0d82ff0
Featherbutt <iamjohndoe220@gmail.com>
2025-07-01 14:36:40 -0700

    Remove unnecessary using directives.

--- LiesOfPRandomizer/Randomizer/Randomizer.cs
+++ LiesOfPRandomizer/Randomizer/Randomizer.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace LiesOfPRandomizer;
 
