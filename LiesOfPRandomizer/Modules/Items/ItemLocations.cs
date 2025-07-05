afb87210945019a0ee2b5b9e746c6ffb59e4b127
Featherbutt <iamjohndoe220@gmail.com>
2025-07-05 15:09:17 -0700

    Move modules into their own directory

--- LiesOfPRandomizer/Modules/Items/ItemLocations.cs
+++ LiesOfPRandomizer/Modules/Items/ItemLocations.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UAssetAPI.UnrealTypes;
-using UAssetAPI.ExportTypes;
-using UAssetAPI.PropertyTypes.Objects;
-using UAssetAPI;
-using UAssetAPI.Unversioned;
-using UAssetAPI.PropertyTypes.Structs;
-using System.Xml.Linq;
-
-using static LiesOfPRandomizer.ItemPriorityMethods;
+﻿using UAssetAPI.UnrealTypes;
 
 namespace LiesOfPRandomizer;
 
